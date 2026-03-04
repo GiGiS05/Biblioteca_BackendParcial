4bc9ff24712754d74ea63a46e76b29fb06d95ac3
Armando Salgado <20245138@esen.edu.sv>
2026-03-04 11:27:38 -0600

    Policies y roles feature: Policies y roles para préstamos y libros. Se recomienda correr composer install y un php artisan migrate y un php artisan db:seed --class=PermissionSeeder

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -10,11 +10,16 @@ use Illuminate\Http\Request;
 
 class LoanController extends Controller
 {
+    public function __construct() {
+        $this->authorizeResource(Loan::class);
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
+        $this->authorize("viewAny", Loan::class);
         $loans = Loan::with('book')->paginate();
 
         return response()->json(LoanResource::collection($loans));
@@ -25,6 +30,7 @@ class LoanController extends Controller
      */
     public function store(StoreLoanRequest $request)
     {
+        $this->authorize('create', Loan::class);
         $book = Book::find($request->input('book_id'));
 
         if (! $book->is_available || $book->available_copies === 0) {
